984a301f03165896befb7c35b1f70c922ead8ecf
swatimonalisa-patra <swatimonalisa8@gmail.com>
2021-01-17 22:30:31 +0530

    added 6 new files

---
+++ 6Aa.c
@@ -0,0 +1,8 @@
+ # include <stdio.h> 
+ int main( )
+{  
+  int  i = 0 ;  
+  for ( ; i ; )  
+  printf ( "Here is some mail for you\n" ) ; 
+  return 0 ; 
+} 
---
+++ 6Ac.c
@@ -0,0 +1,14 @@
+# include <stdio.h> 
+int main( ) 
+{  int  i = 1, j = 1 ;  
+for ( ; ; )  
+{   
+if ( i > 5 )    
+break ;   
+else    
+j += i ;   
+printf ( "%d\n", j ) ;   
+i += j ;    
+return 0 ;  
+} 
+} 
